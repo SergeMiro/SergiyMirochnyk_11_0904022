83fbbcfb278407318bf80dbd0c19cc5a9685936e
MIROCHNYK <76389860+SergeMiro@users.noreply.github.com>
2022-04-10 11:23:28 +0200

    Creation de structure (dossiers) + components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,24 @@
+import { Component, Fragment } from 'react';
+import { Routes } from 'react-router-dom';
+import './App.scss';
+import Header from './Components/Header';
+import Acceuil from './Pages/Acceuil';
+import Apropos from './Pages/Apropos';
+import Erreur from './Pages/Erreur';
+import Router from './Routes/Routes';
 
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     PROJET 11
-    </div>
-  );
+export default class App extends Component {
+
+  render(){
+    return (
+
+      <Fragment>
+        <Header />
+        <Router />
+      </Fragment>
+
+    );
+  }
+
 }
-
-export default App;
