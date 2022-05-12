c6a43cf30809787c4db86ff01920b80dfcca1a29
MIROCHNYK <76389860+SergeMiro@users.noreply.github.com>
2022-05-12 19:21:40 +0200

    Création component accordion

--- my-app/src/Components/Stars/index.js
+++ my-app/src/Components/Stars/index.js
@@ -1,17 +1,21 @@
-import { Component } from "react"
+import { Component } from "react";
+import { ReactComponent as Star }  from '../../assets/images/Star.svg'
+import './Stars.scss'
 
-export default class Evaluations extends Component{
+
+export default class Stars extends Component{
 
    render(){
-      const {id, rating} = this.props
-      const range = [1, 2, 3, 4, 5]
+      const{rating} = this.props
+      const stars = [1,2,3,4,5];
+      return <aside className="rating"> 
+             {
+               stars.map((index) => { 
+                  const color = (index <= rating)  ? '#FF6060' : '#E3E3E3'                   
+                  return <Star key={index} className="rating__img" alt="StarImage" fill={color} />
+               })
+            }          
 
-
-      return <article className="stars" id={id}>
-         {range.map((rangeElem) =>
-         rating >= rangeElem ? <span key={rangeElem.toString()}>☀️</span> : null
-     )}
-
-      </article>
+   </aside>
    }
-} 
+}
